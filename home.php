6506cea105217d6d1fac606e3a2fb56be3f2d02b
saby2000 <93090357+saby2000@users.noreply.github.com>
2021-10-26 14:41:50 +0530

    Homeaddeduserlist

--- home.php
+++ home.php
@@ -122,6 +122,32 @@ table.table-bordered > tbody > tr > td{
     responsive, mobile-first projects on the web.</p>
   </div>
 </div>
+  <?php
+ $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webcoursera";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT  fname, lname FROM auth";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo " Name: " . $row["fname"]. " " . $row["lname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
 <h1 style="text-align:center;">Courses </h1>
 
 <div class="container">
